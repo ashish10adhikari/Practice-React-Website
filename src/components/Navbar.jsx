16480884e305ff1aa7937eec7b568040db38e321
AshishAd <ashish10adhikari@gmail.com>
2024-12-27 23:32:59 +0545

    responsive navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,89 @@
-import React from 'react'
-import { assets } from '../assets/assets'
+import React from "react";
+import { useState, useEffect} from "react";
+import { assets } from "../assets/assets";
 
 const Navbar = () => {
-  return (
-    <div className='absolute top-0 left-0 w-full z-10'>
-        <div className='container mx-auto flex justify-between items-center py-4 px-6 md:px-20 lg:px-32 bg-transparent'>
-            <img src={assets.logo} alt="" />
-            <ul className='hidden md:flex gap-7 text-white'>
-                <a href="#Header" className='cursor-pointer hover:text-gray-400'>Home</a>
-                <a href="#Header" className='cursor-pointer hover:text-gray-400'>About</a>
-                <a href="#Header" className='cursor-pointer hover:text-gray-400'>Projects</a>
-                <a href="#Header" className='cursor-pointer hover:text-gray-400'>Testimonials</a>
-            </ul>
-            <button className='hidden md:block bg-white px-8 py-2 rounded-full'>Sign Up</button>
-        </div>
-    </div>
-  )
-}
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
 
-export default Navbar
+  useEffect(() => {
+    if (showMobileMenu) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, [showMobileMenu]);
+  return (
+    <div className="absolute top-0 left-0 w-full z-10">
+      <div className="container mx-auto flex justify-between items-center py-4 px-6 md:px-20 lg:px-32 bg-transparent">
+        <img src={assets.logo} alt="" />
+        <ul className="hidden md:flex gap-7 text-white">
+          <a href="#Header" className="cursor-pointer hover:text-gray-400">
+            Home
+          </a>
+          <a href="#About" className="cursor-pointer hover:text-gray-400">
+            About
+          </a>
+          <a href="#rojects" className="cursor-pointer hover:text-gray-400">
+            Projects
+          </a>
+          <a
+            href="#Testimonials"
+            className="cursor-pointer hover:text-gray-400"
+          >
+            Testimonials
+          </a>
+        </ul>
+        <button className="hidden md:block bg-white px-8 py-2 rounded-full">
+          Sign Up
+        </button>
+        <img
+          onClick={() => setShowMobileMenu(true)}
+          src={assets.menu_icon}
+          className="md:hidden w-7 cursor-pointer"
+          alt=""
+        />
+      </div>
+      {/*---Mobile--- */}
+      <div
+        className={`md:hidden ${
+          showMobileMenu ? "fixed w-full" : "h-0 w-0"
+        } right-0 top-0 bottom-0 overflow-hidden bg-white transition-all`}
+      >
+        <div className="flex justify-end p-6 cursor-pointer">
+          <img onClick={()=> setShowMobileMenu(false)} src={assets.cross_icon} className="w-6" alt="" />
+        </div>
+        <ul className="flex flex-col items-center font-medium gap-2 mt-5 px-5 text-lg">
+          <a
+            href="#Header"
+            className="px-4 py-2 rounded-full inline-block hover:bg-gray-100"
+          >
+            Home
+          </a>
+          <a
+            href="#About"
+            className="px-4 py-2 rounded-full inline-block hover:bg-gray-100"
+          >
+            About
+          </a>
+          <a
+            href="#Projects"
+            className="px-4 py-2 rounded-full inline-block hover:bg-gray-100"
+          >
+            Projects
+          </a>
+          <a
+            href="#Testimonials"
+            className="px-4 py-2 rounded-full inline-block hover:bg-gray-100"
+          >
+            Testimonials
+          </a>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
