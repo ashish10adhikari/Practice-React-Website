9b36affa766771ac8adb72b019b17ccd3dd3aab9
AshishAd <ashish10adhikari@gmail.com>
2024-12-28 20:43:52 +0545

    added slider

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,12 +1,101 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { assets, projectsData } from "../assets/assets";
 
 const Projects = () => {
-  return (
-    <div>
-        <h1>Projects <span>Completed</span>n</h1>
-        <p>Crafting Spaces, Building Legacies-Explore Our Portfolio</p>
-    </div>
-  )
-}
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [cardstoShow, setCardsToShow] = useState(1);
 
-export default Projects
+  const nextProject = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % projectsData.length);
+  };
+
+  const prevProject = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? projectsData.length - 1 : prevIndex - 1
+    );
+  };
+
+  useEffect(() => {
+    const updatecardsToShow = () => {
+      if (window.innerWidth >= 1024) {
+        setCardsToShow(projectsData.length);
+      } else {
+        setCardsToShow(1);
+      }
+    };
+      updatecardsToShow();
+      window.addEventListener("resize", updatecardsToShow);
+      return () => {
+        window.removeEventListener("resize", updatecardsToShow);
+      };
+    
+  }, []);
+
+  return (
+    <div
+      className="container mx-auto py-4 px-6 md:px-20 lg:px-32 my-20 w-full overflow-hidden"
+      id="Projects"
+    >
+      <h1 className="text-2xl sm:text-4xl font-bold mb-2 text-center">
+        Projects{" "}
+        <span className="underline underline-offset-4 decoration-1 under font-light">
+          Completed
+        </span>
+      </h1>
+      <p className="text-center text-gray-500 mb-8 max-w-80 mx-auto">
+        Crafting Spaces, Building Legacies-Explore Our Portfolio
+      </p>
+      {/* <!-- slider buttons --> */}
+      <div className="flex justify-end items-center mb-8">
+        <button
+          className="p-3 bg-gray-200 rounded mr-2 "
+          aria-label="Previous Project"
+          onClick={prevProject}
+        >
+          <img src={assets.left_arrow} alt="previous" />
+        </button>
+        <button
+          className="p-3 bg-gray-200 rounded mr-2 "
+          aria-label="Next Project"
+          onClick={nextProject}
+        >
+          <img src={assets.right_arrow} alt="next" />
+        </button>
+      </div>
+
+      {/* <!-- slider container --> */}
+
+      <div className="overflow-hidden">
+        <div
+          className="flex gap-8 transition-transform duration-500 ease-in-out"
+          style={{
+            transform: `translateX(-${(currentIndex * 100) / cardstoShow}%)`,
+          }}
+        >
+          {projectsData.map((project, index) => (
+            <div key={index} className="relative flex-shrink-0 w-full sm:w-1/4">
+              <img
+                src={project.image}
+                alt={project.title}
+                className="w-full h-auto mb-14"
+              />
+              <div className="absolute left-0 right-0 bottom-5 flex justify-center">
+                <div className="inline-block bg-white w-3/4 px-4 py-2 shadow-md">
+                  <h2 className="font-semibold text-xl text-gray-800">
+                    {project.title}
+                  </h2>
+                  <p className="text-gray-500 text-sm">
+                    {project.price} <span> | </span>
+                    {project.location}
+                  </p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
