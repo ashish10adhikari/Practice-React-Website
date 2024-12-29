fa07b525acfa647081453b25f8b444a9680c42a0
AshishAd <ashish10adhikari@gmail.com>
2024-12-29 18:33:15 +0545

    Contact completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from './components/Header'
 import About from './components/about'
 import Projects from './components/Projects'
 import Testimonials from './components/testimonials'
+import Contact from './components/Contact'
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <About/>
       <Projects/>
       <Testimonials/>
+      <Contact/>
     </div>
   )
 }
