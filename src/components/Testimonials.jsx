670525703550f506ad0f319882807f135337c243
AshishAd <ashish10adhikari@gmail.com>
2024-12-29 17:10:35 +0545

    added testimonial

---
+++ src/components/Testimonials.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Testimonials = () => {
+  return (
+    <div
+      className="container mx-auto py-10 lg:px-32 w-full overflow-hidden"
+      id="Testimonials"
+    >
+      <h1 className="text-2xl sm:text-4xl font-bold mb-2 text-center">
+        Customer{" "}
+        <span className="underline underline-offset-4 decoration-1 under font-light">
+          Testimonials
+        </span>
+      </h1>
+      <p>Real Stories from Those Who Found Home with Us</p>
+    </div>
+  );
+};
+
+export default Testimonials;
